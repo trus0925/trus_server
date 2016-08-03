561680766ea91ae681733fef731d1a8f941337eb
pengu1215 <pengu1215@gmail.com>
2016-08-03 19:50:38 +0900

    160803_changeRoutersName

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var users = require('./routes/users');
-var buses = require('./routes/buses');
+var schdules = require('./routes/schdules');
 
 var app = express();
 
@@ -19,11 +19,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', users);
-app.use('/buses', buses);
+app.use('/schdules', schdules);
 
-app.listen(3000, function () {
-    console.log('listening... port 3000');
-})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,8 +29,8 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
+// error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -56,5 +53,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.listen(3000, function () {
+    console.log('listening... port 3000');
+})
 
 module.exports = app;
