5b66a67fad9899644e1b20bcee5ea77443d85280
pengu1215 <pengu1215@gmail.com>
2016-08-27 20:53:59 +0900

    160827_modifyAllRoutes3

--- routes/buses.js
+++ routes/buses.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
     connection.query('select num, departure, arrival, tripdate, starttime, endtime, totalcount, fee, IF(isavailable,"true","false") as isavailable from bus order by tripdate asc;', function (error, rows) {
     // connection.query('select * from bus order by tripdate asc;', function (error, rows) {
         
-		res.json({
+        res.json({
             result : "success",
             buses : rows
         });
