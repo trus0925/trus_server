54d8ecc68ca1867a12c4b94b28c6066a8a72af0b
pengu1215 <pengu1215@gmail.com>
2016-08-27 20:44:31 +0900

    160827_modifyAllRoutes2

--- routes/buses.js
+++ routes/buses.js
@@ -12,8 +12,9 @@ var connection = mysql.createConnection({
 
 router.get('/', function(req, res, next) {
     
-    // connection.query('select num, departure, arrival, tripdate, starttime, endtime, totalcount, fee, IF(isavailable,"true","false") as isavailable from bus order by tripdate asc;', function (error, rows) {
-    connection.query('select * from bus order by tripdate asc;', function (error, rows) {
+    connection.query('select num, departure, arrival, tripdate, starttime, endtime, totalcount, fee, IF(isavailable,"true","false") as isavailable from bus order by tripdate asc;', function (error, rows) {
+    // connection.query('select * from bus order by tripdate asc;', function (error, rows) {
+        
 		res.json({
             result : "success",
             buses : rows
