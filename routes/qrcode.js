8c4d3d8a8b28155814a87bb75e034b6e7e83c37a
Yurim Jeong <pengu1215@gmail.com>
2016-09-09 10:53:01 +0900

    160909_setQRcodeBoardingCheck

--- routes/qrcode.js
+++ routes/qrcode.js
@@ -13,25 +13,36 @@ var connection = mysql.createConnection({
 //get passenger information
 router.get('/:userid', function(req, res, next) {
   
-	connection.query('select * from applicant where user_userid=? AND isdeposit=1;', [req.params.userid], function (error, row) {
-		
+	connection.query('SELECT * FROM applicant WHERE user_userid=? AND isdeposit=1;', [req.params.userid], function (error, row) {		
         if (error == null) {
             if(row.length>0){
                 var applicant = row[0];
                 
-                connection.query('select username from user where userid=?;', [req.params.userid], function (error, row) {
-                    if (error == null) {
+                connection.query('SELECT username FROM user WHERE userid=?;', [req.params.userid], function (error, row) {
+                    if (error == null) {                        
                         if(row.length>0){
+                            var username = row[0].username;
                             
-                            var answer = "";
-                            answer += "이름 : " + row[0].username + "\n";
-                            answer += "버스 번호 : " + applicant.bus_num + "\n";
-                            answer += "상세 출발지 : " + applicant.departureplace + "\n";
-                            // answer += "좌석 번호 : " + applicant.seatnumber + "\n"; 
-                            
-                            res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
-                            res.end(answer);            
-                
+                            connection.query('UPDATE applicant SET isboarding=1 WHERE user_userid=?', [req.params.userid], function (err, row) {
+                                
+                                if(error == null){
+                                    
+                                    var answer = "";
+                                    answer += "이름 : " + username + "\n";
+                                    answer += "버스 번호 : " + applicant.bus_num + "\n";
+                                    answer += "상세 출발지 : " + applicant.departureplace + "\n";
+                                    answer += "좌석 번호 : " + applicant.departureplace + "\n"; 
+                                    
+                                    res.writeHead(200, {'Content-Type':'text/plain; charset=utf-8'});
+                                    res.end(answer);        
+                                      
+                                } else {
+                                    res.status(503).json({    
+                                        result : "fail",
+                                        reason : error
+                                    });                    
+                                }
+                            });
                         }
                         
                     } else {
